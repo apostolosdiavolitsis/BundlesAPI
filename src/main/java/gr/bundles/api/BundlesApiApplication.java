ab35613314af2e75508839f04bda685094fd1369
apostolosdiavolitsis <apostolosdiavolitsis@gmail.com>
2019-09-18 09:44:03 +0300

    Create Project

---
+++ src/main/java/gr/bundles/api/BundlesApiApplication.java
@@ -0,0 +1,13 @@
+package gr.bundles.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundlesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BundlesApiApplication.class, args);
+	}
+
+}
