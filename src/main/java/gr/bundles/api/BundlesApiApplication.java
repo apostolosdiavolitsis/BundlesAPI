d9e87d6b24d7659359d9f1dd83b913389f307489
apostolosdiavolitsis <apostolosdiavolitsis@gmail.com>
2019-09-18 09:51:57 +0300

    Create a basic message as a first page

--- src/main/java/gr/bundles/api/BundlesApiApplication.java
+++ src/main/java/gr/bundles/api/BundlesApiApplication.java
@@ -2,12 +2,28 @@ package gr.bundles.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-public class BundlesApiApplication {
+@RestController
+public class BundlesApiApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(BundlesApiApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BundlesApiApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String info() {
+		return "This is a demo REST API for bundled offers";
+	}
+	
 }
